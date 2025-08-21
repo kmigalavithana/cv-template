47e3708a15121974c902c71ee80d5bcbbdb43e28
Kavindu Methmal <kavindumethmal1234@gmail.com>
2025-08-21 23:12:03 +0530

    Insatll prettier and navbar button add the onlicke to continue .

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import { navTab } from "./utilities/fields-data/nav-tab-button.ts";
+import { useState } from "react";
 
-import * as React from "react";
+const App = () => {
 
-const App:React.FC = () => {
-
+  const [activeTab, setActiveTab] = useState<string>('skills');
+  console.log(activeTab);
   return (
-      <div>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </div>
-  )
-}
+    <div className="max-w-7xl mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 gap-8">
+        <div>
+          <nav className="flex space-x-1">
+            {navTab.map((tab) => (
+              <button
+                key={tab.id}
+                className={ `flex flex-1 items-center justify-cente border rounded-b-md ${activeTab === tab.id ? 'bg-blue-500' : 'bg-white'} `}
+                onClick={() => setActiveTab(tab.id)}
 
-export default App
+              >
+                {tab.label}
+              </button>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/utilities/fields-data/nav-tab-button.ts
@@ -0,0 +1,7 @@
+export const navTab = [
+    {id:'template',label:'template'},
+    {id:'personal',label:'Personal'},
+    {id:'education',label:'Education'},
+    {id:'professional',label:'Professional'},
+    {id:'skills',label:'Skills'},
+]

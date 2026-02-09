08f4c05aff330451dc92add5da93f3256259c80a
Kavindu Methmal <136598207+kmigalavithana@users.noreply.github.com>
2026-02-10 01:27:37 +0530

    Catching the redux store state Used Useselector and chnge the click of the actvie state.

--- src/components/TemplateSelector.tsx
+++ src/components/TemplateSelector.tsx
@@ -1,26 +1,27 @@
 import { templates } from "../utilities/fields-data/template.ts";
-import { useDispatch} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setSelectedTemplate } from "../store/cvSlice.ts";
 import React from "react";
 
 const TemplateSelector:React.FC = () => {
 
   const dispatch = useDispatch();
-  // const selectedtemplate = useSelector((state) =>state.cv.selectedTemplate);
-
+  const selectedTemplate = useSelector((state) => state.selectedTemplate);
+  console.log(selectedTemplate);
   return (
     <div>
       <h2 className="text-xl font-bold">Choose template</h2>
       <div className='grid grid-cols-1 gap-6'>
         {templates.map((template) => (
-          <button
+          <div
+            className={selectedTemplate === template.id ?`bg-blue-500`: `bg-white`}
             key={template.id}
-            className="text-xl font-semibold text-gray-400"
+            // className="text-xl font-semibold text-gray-400"
            onClick={() => dispatch(setSelectedTemplate(template.id))}
             // mokakd select kre kiyala blanna tempalt ekai id ekai gatte
           >
             {template.name}
-          </button>
+          </div>
         ))}
       </div>
     </div>

15671255f1855d0cd22abec537271eab9a82696f
Kavindu Methmal <136598207+kmigalavithana@users.noreply.github.com>
2026-03-04 23:55:43 +0530

    Set default axios base URL and implement user sign-in API call

--- src/utilities/api/UserSigninAPI.ts
+++ src/utilities/api/UserSigninAPI.ts
@@ -1,8 +1,11 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
+import  axios from "axios";
 
 const UserSigninAPI = createAsyncThunk(
-    'auth/login'
-    async () => {}
+    'auth/login',
+    async () => {
+        axios.post('api/user-sign-in')
+    }
 )
 
 
--- src/utilities/routes/AppRoutes.tsx
+++ src/utilities/routes/AppRoutes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {Route, Routes} from "react-router";
 import CVTemplateSection from "../../components/CvTemplate/CVTemplateSection.tsx";
 import UserSignIn from "../../components/Signin/UserSignIn.tsx";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 
 const AppRoutes:React.FC = () => {
     return (
